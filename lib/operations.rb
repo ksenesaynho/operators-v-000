30bae19ec4a4c37a36c051cf798fe9f6a3036ddb
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 19:52:08 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,17 @@
 def unsafe?(speed)
-  if 40 < 60
-    "Going 30 mph on the freeway would be unsafe, as would going 95 mph"
-  else
-    ""
-
+  if speed > 60
+    true
+  elsif speed < 40
+  true
+else
+  false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
